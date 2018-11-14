8c743a1d06edbc702bdbbe0ee05f9b379435047d
PatrikPajer <p.pajer@email.cz>
2018-11-14 09:45:31 +0100

    Add sounds for the drumkit

--- Projects/01_Drumkit/script.js
+++ Projects/01_Drumkit/script.js
@@ -1,4 +1,4 @@
 window.addEventListener("keydown", function(e) {
-	let audio = document.querySelector(`audio[data-key=${e.keyCode}]`);
+	let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 	console.log(audio);
 });
