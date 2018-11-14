4f49b7bf2fa7bfd7bfc1b7b3fb506e746838cce1
PatrikPajer <p.pajer@email.cz>
2018-11-14 12:07:35 +0100

    Functionally done

--- Projects/01_Drumkit/script.js
+++ Projects/01_Drumkit/script.js
@@ -1,4 +1,16 @@
 window.addEventListener("keydown", function(e) {
 	let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-	console.log(audio);
+	let key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+	if(!audio) return; //zmáčknutí neasociované klávesy přeruší funkci
+	audio.currentTime = 0; // přetočit na začátek
+	audio.play();
+	key.classList.add("playing");
 });
+
+let removeTransition = function(e) {
+	if(e.propertyName !== "transform") return;
+	this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
