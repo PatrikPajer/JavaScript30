d177dff28b497f119c8e1eddda928a9d579c16b9
PatrikPajer <p.pajer@email.cz>
2018-11-15 19:02:25 +0100

    Functionally done. Problem while moving from the final number of degrees to the starting one. It goes all the way backwards.

--- Projects/02_Clock/script.js
+++ Projects/02_Clock/script.js
@@ -1,3 +1,25 @@
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".minute-hand");
+const hourHand = document.querySelector(".hour-hand");
+
 function setDate() {
-	console.log("Hi");
+	const now = new Date();
+	const seconds = now.getSeconds();
+	const secondsDegrees = ((seconds/60) * 360) + 90;
+	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+	const minutes = now.getMinutes();
+	const minutesDegrees = ((minutes/60) * 360) + 90;
+	minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+	let hours = now.getHours();
+	if(hours > 12) {
+		hours -= 12;
+	};
+	const hoursDegrees = ((hours * 30) + 90);
+	hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+	console.log(hours);
 };
+
+setInterval(setDate, 1000);
