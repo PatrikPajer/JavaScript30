2d0dbbd57d5d690f5a60809d82fa1da11953f2fd
PatrikPajer <p.pajer@email.cz>
2018-11-21 17:58:17 +0100

    Create drawing function, changing colors and width of the stroke

--- Projects/08_Fun_with_HTML_canvas/script.js
+++ Projects/08_Fun_with_HTML_canvas/script.js
@@ -8,3 +8,35 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = "#BADA55";
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
+ctx.lineWidth = 15;
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+
+function draw(e) {
+	if(!isDrawing) return; //stop when not mousedown
+	console.log(e);
+	ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+	ctx.beginPath();
+	ctx.moveTo(lastX, lastY);
+	ctx.lineTo(e.offsetX, e.offsetY);
+	ctx.stroke();
+	lastX = e.offsetX;
+	lastY = e.offsetY;
+	hue++;
+	if(hue > 360) {
+		hue = 0;
+	}
+	ctx.lineWidth++;
+}
+
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mousedown", (e) => {
+	isDrawing = true;
+	lastX = e.offsetX;
+	lastY = e.offsetY;
+});
+canvas.addEventListener("mouseup", () => isDrawing = false);
+canvas.addEventListener("mouseout", () => isDrawing = false);
